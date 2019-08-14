83574860859be38322f1120092012c76c89b5726
David Foote <david.a.foote@gmail.com>
2019-08-14 17:05:26 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -8,13 +8,3 @@ CREATE TABLE bears (
   alive BOOLEAN
   );
   
-  INSERT INTO bears (name) VALUES ('Mr. Chocolate'); 
-  INSERT INTO bears (name) VALUES ('Rowdy'); 
-  INSERT INTO bears (name) VALUES ('Tabitha');
-  INSERT INTO bears (name) VALUES ('Sergeant Brown'); 
-  INSERT INTO bears (name) VALUES ('Melissa');
-  INSERT INTO bears (name) VALUES ('Grinch');
-  INSERT INTO bears (name) VALUES ('Wendy');
-  INSERT INTO bears (name) VALUES (NULL);
-  
-  SELECT * FROM bears WHERE (name, age)
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'male', 'brown', 'happy', 1); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'male', 'black', 'frisky', 1); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'shy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'male', 'tan', 'bossy', 1); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female', 'brown', 'white', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 6, 'male', 'grey', 'grouchy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'female', 'white', 'sleepy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, 'male', 'black', 'happy', 0);
+  
