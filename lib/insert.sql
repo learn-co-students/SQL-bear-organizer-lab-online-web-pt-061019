bd26735a7928ec20f20ff74553c972f6a3c2c6dc
ennalezah <hae.villareal@gmail.com>
2019-08-13 21:56:08 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES
+(9, 'Mr. Chocolate', 13, 'male', 'brown', 'friendly', 1),
+(10, 'Rowdy', 2, 'male', 'brown', 'curious', 1),
+(11, 'Tabitha', 10, 'female', 'black', 'outgoing', 1),
+(12, 'Sergeant Brown', 15, 'female', 'dark brown', 'moody', 0),
+(13,'Melissa', 5, 'female', 'cinnamon', 'friendly', 1),
+(14, 'Grinch', 8, 'male', 'blonde', 'moody', 1),
+(15, 'Wendy', 4, 'female', 'brown', 'goofy', 1),
+(16, NULL, 9, 'male', 'black', 'hangry', 0);
