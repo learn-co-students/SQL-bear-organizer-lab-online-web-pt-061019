0776a3f4797237ae6dd9c91a34011facf2112f5e
Maasa <konomaasa@gmail.com>
2019-08-16 07:39:46 -0700

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'female';"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -11,21 +11,21 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age, MAX(age) FROM bears;"
+  "SELECT name, MAX(age) FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age, MIN(age) FROM bears;"
+  "SELECT name, MIN(age) FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears WHERE color = 'light brown';"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT * FROM bears WHERE temperament = 'goofy';"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = NULL;"
+  "SELECT * FROM bears WHERE id = 8;"
 end
