ce5c106ad70d342357241ec5ddc607795ef18be9
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-22 05:35:39 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,21 +11,21 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Select name, age From bears Desc Limit 1"
+  "Select name, age From bears Desc Limit 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Select name, age From bears Order By age Limit 1"
+  "Select name, age From bears Order By age Limit 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Select color From bears Group By  Limit 1"
+  "Select color, count(color) From bears Group by Color Order by count(*) Desc Limit 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "Select Count(temperament) From bears Where temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "Select * From bears Where name is null;"
 end
